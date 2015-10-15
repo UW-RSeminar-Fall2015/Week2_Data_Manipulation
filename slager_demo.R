862cc6cfd84d92ff943bd1061f275af344da0608
slager <dave.slager@gmail.com>
2015-10-14 17:37:48 -0700

    import the data files

--- slager_demo.R
+++ slager_demo.R
@@ -1,2 +1,6 @@
 ## Import birds
-read.csv("birds.csv") -> birds
+read.csv("birds.csv",header=T,stringsAsFactors=F) -> birds
+
+## Import everett
+read.csv("everett.csv",header=T,stringsAsFactors=F) -> everett
+
