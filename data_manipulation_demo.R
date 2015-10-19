33e5a92e6650a5804a53ebb029b7988e21cb7f76
Jen C <jennifer.cahalan@noaa.gov>
2015-10-19 11:58:26 -0700

    final version of data manipulation and plyr combined to one script

--- data_manipulation_demo.R
+++ data_manipulation_demo.R
@@ -197,7 +197,9 @@ summary.spp.plyr <- ddply(everett, .(species), summarize,
 )
 head(summary.spp.plyr)
 
+####################################################
 ## try some other structures and more complex data
+#options(width = 180)
 names(birds)
 head(birds)
 ## number of birds by species
@@ -226,3 +228,54 @@ bird.summry.dat$pct_Female <- bird.summry.dat$num_birds_F / bird.summry.dat$num_
 bird.summry.dat
 
 ## transform example
+##    add a new variable to existing data frame: pct_fat = fat/mass
+dim(birds)
+head(birds)
+birds <- ddply (birds, .(species, sex), transform,
+		pct_fat = round(fat/mass,2)
+)
+dim(birds)
+head(birds)
+## get the original data file back
+read.csv("birds.csv",header=T,stringsAsFactors=F,na.strings="") -> birds
+head(birds)
+names(birds)
+## or create a new data frame ~ note it has the same number of records as original 
+birds.plus <- ddply (birds, .(species, sex), transform,
+		pct_fat = round(fat/mass,2)
+)
+dim(birds.plus)
+head(birds.plus)
+tail(birds.plus)
+birds.plus[c(1:3,16:18,31:33,46:49,64:66),]
+
+##  add both a new variable (pct_mass) and summary variables number of birds of each gender and mean fat of birds of each gender
+head(birds)
+ddply (birds, .(species, sex), summarize,
+		num_birds = length(id),
+		mean_fat = mean(fat),
+		var_fat = var(fat)
+) ## the group summaries for reference
+
+birds.plus <- ddply (birds, .(species, sex), transform,
+		pct_fat = round(fat/mass,2),
+		num_birds_spp_sex = length(id),
+		mean_fat_spp_sex = round(mean(fat),2)
+)
+dim(birds.plus)
+birds.plus[c(1:3,16:18,31:33,46:49,64:66),]
+head(birds.plus)
+tail(birds.plus)
+
+## Lastly using other data structures
+##    input a dataframe, for each species determine the number of birds, number of each species, the mean and var of fat, output as a list
+bird.list <- dlply(birds, .(species), summarize,
+		num_birds = length(id),
+		num_F = length(id[which(species=='F')]),
+        num_M = length(id[which(species=='M')]),
+		mean_fat = mean(fat),
+		var_fat = var(fat)
+		)
+str(bird.list)
+bird.list
+
